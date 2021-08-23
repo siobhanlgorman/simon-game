8ec77b023e671a93bbde5aa6e6c3891cfcf83f05
siobhanlgorman <siobhanlgorman@gmail.com>
2021-08-23 08:04:19 +0000

    Change text for currentGames test to undo  previous change

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -44,11 +44,11 @@ describe("newGame() works correctly", () => {
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("should clear computer sequence array", () => {
+    test("should clear playerMoves array", () => {
         // expect(game.playerMoves).toEqual([]);
         expect(game.playerMoves.length).toBe(0);
     });
-    test("should clear currentGame array", () => {
+    test("should clear computer sequence array", () => {
         // expect(game.currentGame).toEqual([]);
         expect(game.currentGame.length).toBe(0);
     });
